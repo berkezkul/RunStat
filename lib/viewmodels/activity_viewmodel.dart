f714447dd59d4cfb0c7b09a6e38a3dceea0099c3
Berke Özkul <berkeozkul@posta.mu.edu.tr>
2024-10-31 17:02:33 +0300

    runstat

--- lib/viewmodels/activity_viewmodel.dart
+++ lib/viewmodels/activity_viewmodel.dart
@@ -1,13 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
-
+import 'package:intl/intl.dart';
 import '../data/models/activity_model.dart';
 
 class ActivityViewModel extends ChangeNotifier {
   List<Activity> activities = [];
+  List<Activity> filteredActivities = [];
   bool isLoading = true;
-
   final String userId;
+  String selectedCriterion = 'Date';
+  bool ascendingOrder = true;
 
   ActivityViewModel({required this.userId}) {
     fetchActivities();
@@ -21,19 +23,42 @@ class ActivityViewModel extends ChangeNotifier {
           .collection('runs')
           .get();
 
-      for (var doc in snapshot.docs) {
-        print('Document Data: ${doc.data()}');
-        activities.add(Activity.fromFirestore(doc.data() as Map<String, dynamic>));
-      }
-
       activities = snapshot.docs
           .map((doc) => Activity.fromFirestore(doc.data() as Map<String, dynamic>))
           .toList();
+      filteredActivities = activities;
     } catch (e) {
       print('Error fetching activities: $e');
     } finally {
       isLoading = false;
       notifyListeners();
     }
+  }
+
+  void filterActivities(String searchText) {
+    if (searchText.isEmpty) {
+      filteredActivities = activities;
+    } else {
+      filteredActivities = activities.where((activity) {
+        final formattedDate = DateFormat('dd.MM.yyyy').format(DateTime.parse(activity.date));
+        final distanceString = activity.distance.toString();
+
+        // that matches exactly (e.g., '45' in '450' or '145')
+        final bool distanceMatch = distanceString.contains(RegExp(r'\b' + RegExp.escape(searchText)));
+
+        return distanceMatch || formattedDate.contains(searchText);
+      }).toList();
+    }
+    notifyListeners();
+  }
+
+  void sortByDate(bool ascending) {
+    filteredActivities.sort((a, b) => ascending ? a.date.compareTo(b.date) : b.date.compareTo(a.date));
+    notifyListeners();
+  }
+
+  void sortByDistance(bool ascending) {
+    filteredActivities.sort((a, b) => ascending ? a.distance.compareTo(b.distance) : b.distance.compareTo(a.distance));
+    notifyListeners();
   }
 }
